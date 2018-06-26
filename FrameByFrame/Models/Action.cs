2078fd7db8f651f4f3a3ee271197823fa2429017
djdcann <djdcann@gmail.com>
2018-06-26 11:00:43 -0400

    update that shit

---
+++ FrameByFrame/Models/Action.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrameByFrame.Models
+{
+    public abstract class Action
+    {
+        string Name { get; set; }
+        ActionType type { get; set; }
+        int[] StateChange { get; set; }
+        int Total { get; set; }
+        int FAF { get; set; }
+    }
+
+    public enum ActionType
+    {
+        Normal,
+        Smash,
+        Aerial,
+        Special,
+        Grab,
+        Throw,
+        Evasive
+    }
+}
---
+++ FrameByFrame/Models/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrameByFrame.Models
+{
+    public class Character
+    {
+        int ID;
+        string Name;
+        string imageFile;
+        object Properties { get; set; }
+        List<Action> Actions { get; set; }
+    }
+}
