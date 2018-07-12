ad8007bcba2c299e2a1af9f33ae3f9b79f7abe10
Djdcann <cannistrarod@wit.edu>
2018-07-12 13:32:40 -0400

    updated Character Model

--- FrameByFrame/Models/Character.cs
+++ FrameByFrame/Models/Character.cs
@@ -10,10 +10,56 @@ namespace FrameByFrame.Models
     {
         private string imagePath = "/images/portraits/";
 
-        int ID { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
         protected string ImageFile { get; set; }
-        public CharacterProperties Properties { get; set; }
+        public CharacterProperties Attributes { get; set; }
+
+        public Evasive Airdodge { get; set; }
+        public Evasive Spotdodge { get; set; }
+        public Evasive RollF { get; set; }
+        public Evasive RollB { get; set; }
+        public Grab Grab { get; set; }
+        public Grab GrabD { get; set; }
+        public Normal TiltU { get; set; }
+        public Normal TiltF { get; set; }
+        public Normal TiltD { get; set; }
+        public Smash SmashU { get; set; }
+        public Smash SmashF { get; set; }
+        public Smash SmashD { get; set; }
+        public Aerial AerialU { get; set; }
+        public Aerial AerialF { get; set; }
+        public Aerial AerialD { get; set; }
+        public Aerial AerialB { get; set; }
+        public Aerial AerialN { get; set; }
+
+        public IEnumerable<Special> Specials { get; set; }
+
+        public IEnumerable<Normal> Normals
+        {
+            get { return new List<Normal>() { this.TiltU, this.TiltF, this.TiltD }; }
+        }
+        public IEnumerable<Smash> Smashes
+        {
+            get { return new List<Smash>() { this.SmashU, this.SmashF, this.SmashD }; }
+        }
+        public IEnumerable<Aerial> Aerials
+        {
+            get { return new List<Aerial>() { this.AerialU, this.AerialF, this.AerialD, this.AerialB, this.AerialN }; }
+        }
+
+        public IEnumerable<Action> getAttacks()
+        {
+            List<Action> a = new List<Action>();
+            a.AddRange(Normals);
+            a.AddRange(Smashes);
+            a.AddRange(Aerials);
+            a.AddRange(Specials);
+            a.Add(Grab);
+            a.Add(GrabD);
+            return a;
+        }
+
         public List<Action> Actions { get; set; }
 
         public Character(int ID, string Name, string ImageFile)
@@ -21,8 +67,27 @@ namespace FrameByFrame.Models
             this.ID = ID;
             this.Name = Name;
             this.ImageFile = ImageFile;
-            this.Properties = new CharacterProperties();
+            this.Attributes = new CharacterProperties();
             this.Actions = new List<Action>();
+        }
+
+        public Character(){}
+
+        public Character(string name)
+        {
+            Character c = this.fromJson(System.IO.File.ReadAllText(@"C:\Users\Derek\Desktop\SP\" + name+".json"));
+
+            this.ID = c.ID;
+            this.Name = c.Name;
+            this.ImageFile = c.ImageFile;
+            this.Attributes = c.Attributes;
+            this.Actions = c.Actions;
+        }
+
+        private Character fromJson(string json)
+        {
+            Character c = JsonConvert.DeserializeObject<Character>(json);
+            return c;
         }
 
         public string getJson()
--- FrameByFrame/Models/NormalAttack.cs
+++ FrameByFrame/Models/NormalAttack.cs
@@ -5,37 +5,37 @@ using System.Threading.Tasks;
 
 namespace FrameByFrame.Models
 {
-    public class NormalAttack : Action
+    public class Normal : Action
     {
-        public NormalAttack(string Name, int Total, int[] StateChange, int FAF) : base(Name, Total, StateChange, FAF, ActionType.Normal)
+        public Normal(string Name, int Total, int[] StateChange, int FAF) : base(Name, Total, StateChange, FAF, ActionType.Normal)
         {
 
         }
     }
 
-    public class SmashAttack : Action
+    public class Smash : Action
     {
         public int ChargeFrame { get; set; }
-        public SmashAttack(string Name, int Total, int[] StateChange, int FAF, int ChargeFrame) : base(Name, Total, StateChange, FAF, ActionType.Smash)
+        public Smash(string Name, int Total, int[] StateChange, int FAF, int ChargeFrame) : base(Name, Total, StateChange, FAF, ActionType.Smash)
         {
             this.ChargeFrame = ChargeFrame;
         }
     }
 
-    public class AerialAttack : Action
+    public class Aerial : Action
     {
         public int LandLag { get; set; }
         public int[] CancelRange { get; set; }
-        public AerialAttack(string Name, int Total, int[] StateChange, int FAF, int LandLag, int[] CancelRange) : base(Name, Total, StateChange, FAF, ActionType.Aerial)
+        public Aerial(string Name, int Total, int[] StateChange, int FAF, int LandLag, int[] CancelRange) : base(Name, Total, StateChange, FAF, ActionType.Aerial)
         {
             this.LandLag = LandLag;
             this.CancelRange = CancelRange;
         }
     }
 
-    public class SpecialAttack : Action
+    public class Special : Action
     {
-        public SpecialAttack(string Name, int Total, int[] StateChange, int FAF) : base(Name, Total, StateChange, FAF, ActionType.Special)
+        public Special(string Name, int Total, int[] StateChange, int FAF) : base(Name, Total, StateChange, FAF, ActionType.Special)
         {
 
         }
@@ -44,6 +44,14 @@ namespace FrameByFrame.Models
     public class Grab : Action
     {
         public Grab(string Name, int Total, int[] StateChange, int FAF) : base(Name, Total, StateChange, FAF, ActionType.Grab)
+        {
+
+        }
+    }
+
+    public class Evasive : Action
+    {
+        public Evasive(string Name, int Total, int[] StateChange, int FAF) : base(Name, Total, StateChange, FAF, ActionType.Evasive)
         {
 
         }
