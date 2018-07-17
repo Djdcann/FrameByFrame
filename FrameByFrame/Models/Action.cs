91aa923c6fc85e0b03effe9c76da75098f7863be
Djdcann <cannistrarod@wit.edu>
2018-07-17 18:52:50 -0400

    dynamically make ganttcharts

--- FrameByFrame/Models/Action.cs
+++ FrameByFrame/Models/Action.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace FrameByFrame.Models
@@ -13,7 +14,8 @@ namespace FrameByFrame.Models
         public int[] StateChange { get; set; }
         public int FAF { get; set; }
 
-        private int characterID;
+        protected string lagClass = "lag";
+        protected string activeClass = "hit";
         protected static readonly int dropShield = 15;
         protected static readonly int dropShieldPerfect = 1;
 
@@ -34,6 +36,28 @@ namespace FrameByFrame.Models
         public virtual int getEndlag()
         {
             return FAF - StateChange[StateChange.Length - 1];
+        }
+
+        public virtual string ganttChart()
+        {
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+            sb.Append("<table class='gantt'><tr>");
+            sb2.Append("</tr><tr>");
+            int s = 1;
+            for(int i=0;i<=StateChange.Length;i++)
+            {
+                int a = i < StateChange.Length ? StateChange[i] : FAF;
+                int l = a - s;
+                string c = i % 2 == 0 ? lagClass : activeClass;
+                sb.Append($"<td class='{c}' width='{(float)l*100/FAF}%'>{l}</td>");
+                sb2.Append($"<td>{s}</td>");
+                s = a;
+            }
+            sb2.Append($"<td>{FAF}</td>");
+            sb.Append(sb2);
+            sb.Append("</tr></table>");
+            return sb.ToString();
         }
     }
 
