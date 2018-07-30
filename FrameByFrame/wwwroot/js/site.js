d5af3f0cb2830e229644bab3111d36012bb857f3
Djdcann <cannistrarod@wit.edu>
2018-07-29 22:14:23 -0400

    datatables update

--- FrameByFrame/wwwroot/js/site.js
+++ FrameByFrame/wwwroot/js/site.js
@@ -1,1 +1,16 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function () {
+    $('#fw').DataTable({
+        "paging": false,
+        "searching": true,
+        "columns": [
+            { "orderable": true, "type": "string" },
+            { "orderable": true, "type": "num" },
+            { "orderable": true, "type": "num" },
+            { "orderable": true, "type": "string" }
+        ],
+        "order": [1, 'desc']
+    });
+
+
+});
